8480379b4ae1ec47fb880c4ee86e73c8c12d0f5f
marceloabbadia <marcelo.abbadia@gmail.com>
2023-11-30 12:42:04 -0300

    inserindo regras do negocio da API-Financeira

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "testando o get" });
-});
+app.use(express.json());
 
 app.listen(3333);
